21e61ba7a6ba5c9f6308adf9e09f083518586a7a
201913 <bozinandrey1@yandex.ru>
2022-03-09 09:38:49 +0300

    edit depo

--- BusStations/Depo.cs
+++ BusStations/Depo.cs
@@ -64,6 +64,13 @@ namespace BusStations
                 result += $"Stop {station}: ";
                 foreach (var bus in Stations[station])
                 {
+                    if (Stations[station].Count == 1)
+                    {
+                        result += "no interchange";
+                        break;
+                    }
+                    if (bus == busName)
+                        continue;
                     result += $"{bus} ";
                 }
                 result.Trim();
@@ -71,82 +78,6 @@ namespace BusStations
             }
 
             return result.Trim();
-        }
-    }
-
-    public class City
-    {
-        public Dictionary<string, HashSet<string>> Transport { get; set; }
-        public Dictionary<string, HashSet<string>> Stations { get; set; }
-
-        public City()
-        {
-            Transport = new Dictionary<string, HashSet<string>>();
-            Stations = new Dictionary<string, HashSet<string>>();
-        }
-
-        public void AddTransport(string route)
-        {
-            var transport = route.Split(':')[0];
-            if (!Transport.ContainsKey(transport))
-            {
-                Transport[transport] = new HashSet<string>();
-            }
-            foreach (var station in route.Split(':')[1].Split(','))
-            {
-                var tempStation = station.Trim();
-                Transport[transport].Add(tempStation);
-                if (!Stations.ContainsKey(tempStation))
-                {
-                    Stations[tempStation] = new HashSet<string>();
-                }
-                Stations[tempStation].Add(transport);
-            }
-        }
-
-        public void AddStation(string route)
-        {
-            var station = route.Split(':')[0];
-            if (!Stations.ContainsKey(station))
-            {
-                Stations[station] = new HashSet<string>();
-            }
-            foreach (var bus in route.Split(':')[1].Split(','))
-            {
-                var tempTransport = bus.Trim();
-                Stations[station].Add(tempTransport);
-                if (!Transport.ContainsKey(tempTransport))
-                {
-                    Transport[tempTransport] = new HashSet<string>();
-                }
-                Transport[tempTransport].Add(station);
-            }
-        }
-
-        public string[] GetStations(string bus)
-        {
-            if (Transport.ContainsKey(bus))
-            {
-                foreach (var station in Transport[bus])
-                {
-                    Console.WriteLine(station);
-                }
-            }
-            return Transport[bus].ToArray();
-        }
-        public void GetBuses(string station)
-        {
-            if (Stations.ContainsKey(station))
-            {
-                foreach (var bus in Stations[station])
-                {
-                    Console.WriteLine(bus);
-                }
-            }
-        }
-        public string[] GetAllBuses()
-        {
-            return Transport.Keys.ToArray();
         }
     }
 }
